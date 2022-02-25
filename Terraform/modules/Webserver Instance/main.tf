b42fe79b88bbb72f3047690c3eb0dec7eb703495
Paul-Kehnel <paul.kehnel@ocean.ibm.com>
2022-02-25 16:14:31 +0100

    :loud_sound: Introducing Changelog for better loggin ;)

---
+++ Terraform/modules/Webserver Instance/main.tf
@@ -0,0 +1,156 @@
+locals {
+  naming_prefix = "${var.env}-${var.stage}"
+  location      = data.azurerm_resource_group.rg.location
+}
+
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+data "azurerm_key_vault" "vault" {
+  name                = var.vault_name
+  resource_group_name = var.resource_group_name
+}
+
+data "azurerm_log_analytics_workspace" "log_ws" {
+  name                = "${local.naming_prefix}-${var.log_ws_name}"
+  resource_group_name = var.resource_group_name
+}
+
+data "azurerm_subnet" "subnet" {
+  name                 = "${local.naming_prefix}-subnet_${var.subnet_name != null ? var.subnet_name : var.virtual_server_name}"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.virtual_network_name
+}
+
+#Create an availability set with two fault/update domains, so each webserver is placed into its own domain
+resource "azurerm_availability_set" "avset" {
+  name                         = "${local.naming_prefix}-${var.virtual_server_name}-avset"
+  resource_group_name          = var.resource_group_name
+  location                     = local.location
+  platform_fault_domain_count  = 2
+  platform_update_domain_count = 2
+  managed                      = true
+}
+
+resource "azurerm_virtual_machine" "virtual_servers" {
+  count                 = var.virtual_server_count
+  name                  = "${local.naming_prefix}-${var.virtual_server_name}-${count.index}"
+  location              = local.location
+  resource_group_name   = var.resource_group_name
+  availability_set_id   = azurerm_availability_set.avset.id
+  network_interface_ids = [element(azurerm_network_interface.nic.*.id, count.index)]
+  vm_size               = var.vm_size
+
+  # Delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  storage_image_reference {
+    publisher = var.vm_image.publisher
+    offer     = var.vm_image.offer
+    sku       = var.vm_image.sku
+    version   = var.vm_image.version
+  }
+
+  storage_os_disk {
+    name              = "disk-${count.index}-${var.virtual_server_name}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  # User creation for Ansible
+  os_profile {
+    computer_name  = "${var.virtual_server_name}-${count.index}"
+    admin_username = "${local.naming_prefix}-${var.virtual_server_name}-admin"
+    admin_password = azurerm_key_vault_secret.virtual_server_secret.value
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+    ssh_keys {
+      key_data = var.public_ssh_key
+      path     = "/home/${local.naming_prefix}-${var.virtual_server_name}-admin/.ssh/authorized_keys"
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+  tags = var.standard_tags
+}
+
+# Create FrontEnd NICs for the webservers in the web subnet
+resource "azurerm_network_interface" "nic" {
+  count               = var.virtual_server_count
+  name                = "nic-${local.naming_prefix}-${var.virtual_server_name}-${count.index}"
+  location            = local.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "IPConfiguration"
+    subnet_id                     = data.azurerm_subnet.subnet.id
+    private_ip_address_allocation = "dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "vm_ext_web" {
+  count                      = var.virtual_server_count
+  name                       = "OmsAgentForLinux"
+  virtual_machine_id         = azurerm_virtual_machine.virtual_servers[count.index].id
+  publisher                  = "Microsoft.EnterpriseCloud.Monitoring"
+  type                       = "OmsAgentForLinux"
+  type_handler_version       = "1.12"
+  auto_upgrade_minor_version = true
+
+  settings = <<SETTINGS
+    {
+        "workspaceId": "${data.azurerm_log_analytics_workspace.log_ws.workspace_id}"
+    }
+  SETTINGS
+
+  protected_settings = <<PROTECTEDSETTINGS
+    {
+        "workspaceKey": "${data.azurerm_log_analytics_workspace.log_ws.primary_shared_key}"
+    }
+  PROTECTEDSETTINGS
+}
+
+resource "azurerm_virtual_machine_extension" "da_web" {
+  count                      = var.virtual_server_count
+  name                       = "DAExtension"
+  virtual_machine_id         = azurerm_virtual_machine.virtual_servers[count.index].id
+  publisher                  = "Microsoft.Azure.Monitoring.DependencyAgent"
+  type                       = "DependencyAgentLinux"
+  type_handler_version       = "9.5"
+  auto_upgrade_minor_version = true
+}
+
+# Generate a password for webserver
+resource "random_string" "virtual_server_password" {
+  length      = 14
+  min_upper   = 2
+  min_lower   = 2
+  min_numeric = 2
+  min_special = 2
+}
+
+resource "azurerm_key_vault_secret" "virtual_server_secret" {
+  name         = "${local.naming_prefix}-secret-${var.virtual_server_name}"
+  value        = random_string.virtual_server_password.result
+  key_vault_id = data.azurerm_key_vault.vault.id
+}
+
+resource "azurerm_virtual_machine_extension" "startup" {
+  count                = var.virtual_server_name != "ansible" ? 0 : 1
+  name                 = "startup"
+  virtual_machine_id   = azurerm_virtual_machine.virtual_servers[count.index].id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
+
+  protected_settings = <<PROT
+    {
+        "script": "${base64encode(data.template_file.init_script[0].rendered)}"
+    }
+    PROT
+}
---
+++ Terraform/modules/Webserver Instance/variables.tf
@@ -0,0 +1,42 @@
+variable "env" {}
+variable "stage" {}
+variable "vault_name" {}
+variable "resource_group_name" {}
+variable "virtual_network_name" {}
+
+variable "subnet_name" { default = null }
+variable "virtual_server_name" { default = "webserver" }
+variable "virtual_server_count" { default = 1 }
+
+variable "log_ws_name" { default = "loganalyticsWS" }
+variable "script" { default = "install-upd-upg.sh" }
+
+variable "vm_size" {
+  description = "Specifies the size of the Virtual Machine e.g. Standard_D4_v3. See also Azure VM Naming Conventions"
+  # https://docs.microsoft.com/en-gb/azure/virtual-machines/sizes-general)
+  # https://docs.microsoft.com/en-gb/azure/virtual-machines/vm-naming-conventions
+  default = "Standard_DS1_v2"
+}
+
+variable "vm_image" {
+  description = "Define the OS (publisher, offer, sku, version)"
+  # Publisher: Ubuntu → Canonical; RedHat → RedHat; CentOS → OpenLogic; SuSE Linux → SUSE; Debian → credativ; Oracle Linux → Oracle-Linux; CoreOS → CoreOS
+  # The other 3 attributes can be found via:
+  # az vm image list -f *Publisher_Name* --all
+  default = {
+    "publisher" = "Canonical"
+    "offer"     = "UbuntuServer"
+    "sku"       = "18.04-LTS"
+    "version"   = "latest"
+  }
+}
+
+variable "public_ssh_key" {
+  default = {}
+}
+
+variable "standard_tags" {
+  default     = {}
+  description = "Additional resource tags"
+  type        = map(string)
+}
