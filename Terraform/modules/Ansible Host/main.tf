6e6c569b859bbe25bd0651172d509d9e4c1901ab
Paul-Kehnel <paul.kehnel@ocean.ibm.com>
2022-02-28 17:45:01 +0100

    secret handling

--- Terraform/modules/Ansible Host/main.tf
+++ Terraform/modules/Ansible Host/main.tf
@@ -21,14 +21,14 @@ data "azurerm_key_vault_secret" "private_ssh_key" {
 
 data "template_file" "init_script" {
   template = file("${path.module}/${var.script}")
-  vars = {
+  vars     = {
     ssh_private_key = data.azurerm_key_vault_secret.private_ssh_key.value
-    secret = data.azurerm_key_vault_secret.azure_credentials_secret
-    subscription_id=data.azurerm_key_vault_secret.azure_credentials_subscription_id
-    client_id=data.azurerm_key_vault_secret.azure_credentials_client_id
-    tenant=data.azurerm_key_vault_secret.azure_credentials_tenant
-    pat_token= data.azurerm_key_vault_secret.Devops_PAT
-    userName = "${local.naming_prefix}-${var.virtual_server_name}-admin"
+    secret          = data.azurerm_key_vault_secret.azure_credentials_secret
+    subscription_id = data.azurerm_key_vault_secret.azure_credentials_subscription_id
+    client_id       = data.azurerm_key_vault_secret.azure_credentials_client_id
+    tenant          = data.azurerm_key_vault_secret.azure_credentials_tenant
+    pat_token       = data.azurerm_key_vault_secret.Devops_PAT
+    userName        = "${local.naming_prefix}-${var.virtual_server_name}-admin"
   }
 }
 
@@ -188,11 +188,9 @@ data "azurerm_key_vault_secret" "azure_credentials_tenant" {
 }
 
 data "azurerm_key_vault_secret" "Devops_PAT" {
-  name         = "pat_token"
+  name         = "pattoken"
   key_vault_id = data.azurerm_key_vault.infra-vault.id
 }
-
-
 
 
 # Create User Managed Identity
